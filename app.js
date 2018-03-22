241448db861d9024193b910c6ae14f1f74511ddd
Sebastian Gomez <seagomezar@gmail.com>
2018-03-22 12:17:22 -0500

    Adding the proper way to play a song with tone

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
     const synth = new Tone.Synth().toMaster();
-    
-    const allNotes = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", 
-                      "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", 
-                      "C6"];
+
+    const allNotes = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+        "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+        "C6"];
 
     const piano = document.getElementById("piano");
-    
+
     const widthPerNote = window.innerWidth / (allNotes.length - 4);
     const heightPerNote = 120;
-    
+
     const blackNotesWidthPortion = 2;
     const blackNotesHeightPortion = 1.6;
 
@@ -21,54 +21,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         if (~note.indexOf("#")) {
             node.className = "black";
-            node.style.width = (widthPerNote/blackNotesWidthPortion) + "px";
-            node.style.height = (heightPerNote/blackNotesHeightPortion) + "px";
+            node.style.width = (widthPerNote / blackNotesWidthPortion) + "px";
+            node.style.height = (heightPerNote / blackNotesHeightPortion) + "px";
         } else {
             node.className = "white";
             node.style.width = widthPerNote + "px";
             node.style.height = heightPerNote + "px";
         }
 
-        // Option 1 with predefined note duration
-        // https://github.com/Tonejs/Tone.js/wiki/Time
-
-        node.addEventListener("click", (event)=> {
-            event.target.classList.add('pressed');
-            synth.triggerAttackRelease(note, "4n");
-            setTimeout(() => {
-                event.target.classList.remove("pressed");
-            }, 500);
-        });
-
-        /* Option 2 with human release time
-        node.addEventListener("mousedown", (event)=> {
-            event.target.classList.add('pressed')
-            synth.triggerAttack(note);
-        });
-
-        node.addEventListener("mouseup", (event)=> {
-            event.target.classList.remove("pressed");
-            synth.triggerRelease();
-        });*/
-        
         piano.appendChild(node);
     });
-
-    // Option 1 with no DOM intervention
-    /*
-    const CMajorScale = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
-    let index = 0;
-    let delay = 1000;
-    CMajorScale.forEach((note)=> {
-        setTimeout(() => {
-            synth.triggerAttackRelease(note, "4n");
-        }, delay*index);
-        index++;
-    });*/
-
-    // Option 2 with DOM intervention
-    const CMajorScale = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
-    const DMajorScale = ["D4", "E4", "F#4", "G4", "A4", "B4", "C#5", "D5"];
 
     const MAJOR_SCALES = [
         ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"],
@@ -79,26 +41,26 @@ document.addEventListener("DOMContentLoaded", ()=>{
         ["A4", "B4", "C#5", "D5", "E5", "F#5", "G#5", "A5"],
         ["B4", "C#5", "D#5", "E5", "F#5", "G#5", "A#5", "B5"]
     ];
-    let index = 0;
-    MAJOR_SCALES.forEach(scale => {
-        const transformedScale = scale.concat(scale.slice().reverse());
-        setTimeout(() => {
-            playScale(transformedScale, 500);
-        }, index*transformedScale.length*500);
-        index++;
-    });
-    
+
+    const song = [
+        ["0", "C3", "0.5" ],
+        ["0:2", "C4", "4"]
+    ];
+
+    playSong(song);
+
+    function playSong(song) {
+        Tone.Transport.cancel();
+        Tone.Transport.clear();
+        new Tone.Part((time, note, duration) => {
+            synth.triggerAttackRelease(note, duration, time);
+        }, song).start(0);
+        Tone.Transport.start();
+    }
 
 });
 
-function playScale(scale, delay = 1000) {
-    let index = 0;
 
-    scale.forEach((note)=> {
-        setTimeout(()=>{
-            document.getElementById(note).click();
-        }, delay*index);
-        index++;
-    });
 
-}
+
+
