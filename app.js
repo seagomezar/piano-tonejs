28dde8cfa76dabeb1a1bec4c500cb1f493b4ad8b
Sebastian Gomez <seagomezar@gmail.com>
2018-03-22 11:47:52 -0500

    Adding more scales and playing with them in reverse

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     const synth = new Tone.Synth().toMaster();
     
-    const allNotes = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5"];
+    const allNotes = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", 
+                      "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", 
+                      "C6"];
 
     const piano = document.getElementById("piano");
     
@@ -29,13 +31,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         // Option 1 with predefined note duration
         // https://github.com/Tonejs/Tone.js/wiki/Time
-        /*
-        node.addEventListener("click", (event)=> {
-            synth.triggerAttackRelease(note, "3");
-        });
-        */
 
-        // Option 2 with human release time
+        node.addEventListener("click", (event)=> {
+            event.target.classList.add('pressed');
+            synth.triggerAttackRelease(note, "4n");
+            setTimeout(() => {
+                event.target.classList.remove("pressed");
+            }, 500);
+        });
+
+        /* Option 2 with human release time
         node.addEventListener("mousedown", (event)=> {
             event.target.classList.add('pressed')
             synth.triggerAttack(note);
@@ -44,7 +49,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         node.addEventListener("mouseup", (event)=> {
             event.target.classList.remove("pressed");
             synth.triggerRelease();
-        });
+        });*/
         
         piano.appendChild(node);
     });
@@ -61,4 +66,22 @@ document.addEventListener("DOMContentLoaded", ()=>{
         index++;
     });*/
 
+    // Option 2 with DOM intervention
+    const CMajorScale = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
+    const DMajorScale = ["D4", "E4", "F#4", "G4", "A4", "B4", "C#5", "D5"];
+    
+    playScale(DMajorScale.concat(DMajorScale.slice().reverse()), 500);
+
 });
+
+function playScale(scale, delay = 1000) {
+    let index = 0;
+
+    scale.forEach((note)=> {
+        setTimeout(()=>{
+            document.getElementById(note).click();
+        }, delay*index);
+        index++;
+    });
+
+}
